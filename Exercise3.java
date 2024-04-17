94ba9221d0b62d372ed8d1afb99cd9d86fda3963
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-17 19:44:40 +0100

    Create Exercise3.java

---
+++ Exercise3.java
@@ -0,0 +1,12 @@
+// 3. Write a Java program to divide two numbers and print them on the screen.
+// Test Data :
+// 50/3
+// Expected Output :
+// 16
+
+public class Main{
+    public static void main(String[] args) {
+       int x = 50/3;
+       System.out.println(x);
+    }
+}
