2b06cf0df3b390f5b8ec9e9210182d3ca6f26b06
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-17 19:40:51 +0100

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,13 @@
+// 1. Write a Java program to print 'Hello' on screen and your name on a separate line.
+// Expected Output :
+// Hello
+// Alexandra Abramov
+
+
+public class Main{
+    public static void main(String[] args) {
+        String name = "Your Name";
+        System.out.println("Hello\n" + name);
+    }
+
+}
