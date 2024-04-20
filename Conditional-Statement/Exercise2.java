0c59ea32743e19cfd9e227bd3f37354b0b7c6436
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-20 17:45:29 +0100

    Create Exercise2.java

---
+++ Conditional-Statement/Exercise2.java
@@ -0,0 +1,35 @@
+// 2. Write a Java program to solve quadratic equations (use if, else if and else).
+
+// Test Data
+// Input a: 1
+// Input b: 5
+// Input c: 1
+// Expected Output :
+// The roots are -0.20871215252208009 and -4.7912878474779195
+
+import java.util.Scanner;
+
+public class Main{
+    private static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Input a: ");
+        double a = sc.nextDouble();
+        System.out.print("Input b: ");
+        double b = sc.nextDouble();
+        System.out.print("Input c: ");
+        double c = sc.nextDouble();
+
+        double result = b * b - 4.0 * a * c;
+
+        if (result > 0.0) {
+            double r1 = (-b + Math.pow(result, 0.5)) / (2.0 * a);
+            double r2 = (-b - Math.pow(result, 0.5)) / (2.0 * a);
+            System.out.println("The roots are " + r1 + " and " + r2);
+        } else if (result == 0.0) {
+            double r1 = -b / (2.0 * a);
+            System.out.println("The root is " + r1);
+        } else {
+            System.out.println("The equation has no real roots.");
+        }
+    }
+}
