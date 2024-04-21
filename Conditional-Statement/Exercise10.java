b794fb156b100173d42872163fbe825b03ba33af
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-21 19:10:16 +0100

    Create Exercise10.java

---
+++ Conditional-Statement/Exercise10.java
@@ -0,0 +1,26 @@
+// 10. Write a Java program to display the first 10 natural numbers.
+
+// Expected Output :
+
+// The first 10 natural numbers are:                                                
+                                                                                 
+// 1                                                                                
+// 2                                                                                
+// 3                                                                                
+// 4                                                                                
+// 5                                                                                
+// 6                                                                                
+// 7                                                                                
+// 8                                                                                
+// 9                                                                                
+// 10
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The first 10 natural numbers are:");
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
