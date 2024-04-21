dbe9d5520ecdf759675c93418727de6140175313
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-21 19:02:18 +0100

    Create Exercise8.java

---
+++ Conditional-Statement/Exercise8.java
@@ -0,0 +1,41 @@
+// 8. Write a Java program that requires the user to enter a single character from the alphabet. 
+// Print Vowel or Consonant, depending on user input.
+// If the user input is not a letter (between a and z or A and Z), or is a string of length > 1, print an error message.
+
+// Test Data
+// Input an alphabet: p
+// Expected Output :
+// Input letter is Consonant
+
+
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Input an alphabet: ");
+        String input = sc.next().toLowerCase();
+
+        boolean uppercase = input.charAt(0) >= 65 && input.charAt(0) <= 90;
+        boolean lowercase = input.charAt(0) >= 97 && input.charAt(0) <= 122;
+        boolean vowels = input.equals("a") || input.equals("e") || input.equals("i")
+                || input.equals("o") || input.equals("u");
+
+        if (input.length() > 1)
+        {
+            System.out.println("Error. Not a single character.");
+        }
+        else if (!(uppercase || lowercase))
+        {
+            System.out.println("Error. Not a letter. Enter uppercase or lowercase letter.");
+        }
+        else if (vowels)
+        {
+            System.out.println("Input letter is Vowel");
+        }
+        else
+        {
+            System.out.println("Input letter is Consonant");
+        }
+    }
+}
