ff9b7857bfadb5eeec360f45b4785892e0445449
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-23 15:57:29 +0100

    Create Exercise16.java

---
+++ Basic-1/Exercise16.java
@@ -0,0 +1,19 @@
+// 16. Write a Java program to print a face.
+// Expected Output
+
+//  +"""""+                                                 
+// [| o o |]                                                
+//  |  ^  |                                                 
+//  | '-' |                                                 
+//  +-----+
+
+public class Main{
+    public static void main(String[] args){
+        String face = " +\"\"\"\"\"+\n" +
+                      "[| o o |]\n" +
+                      " |  ^  |\n" +
+                      " | '-' |\n" +
+                      " +-----+";
+        System.out.println(face);
+    }
+}
