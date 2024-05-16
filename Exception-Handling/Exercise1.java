12b0807bf15d05bf83f8d5147f2efaa4acb055c1
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-05-16 18:30:58 +0100

    Create Exercise1.java

---
+++ Exception-Handling/Exercise1.java
@@ -0,0 +1,22 @@
+// 1. Write a Java program that throws an exception and catch it using a try-catch block.
+
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        try{
+            int result = divideNumbers(5, 0);
+            System.out.println("Result: " + result);
+        } catch(ArithmeticException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+    public static int divideNumbers(int dividend, int divisor){
+        if(divisor == 0){
+            throw new ArithmeticException("Cannot divide the given number by zero!");
+        }
+        return dividend / divisor;
+    }
+}
